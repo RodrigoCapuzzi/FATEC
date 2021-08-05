28abc4f5a0ce05725f09b03cc208afa305d1c906
Rodrigo Sanches Dias Capuzzi <70242653+RodrigoCapuzzi@users.noreply.github.com>
2021-08-04 22:20:43 -0300

    Update aula1_ex1.c

--- semestre_3/Estrutura_de_dados/aula1_ex1.c
+++ semestre_3/Estrutura_de_dados/aula1_ex1.c
@@ -1,16 +1,17 @@
 /*
-Atividade 01 – Estrutura de Dados
+DUPLA
+Nome: Diego Mendes Carloni    R.A.:2040482023018   Data: 02/08/2020
+Nome: Rodrigo Sanches Dias    R.A.:2040482023037
 
+Atividade 01 – Estrutura de Dados
 
 1-crie um programa em linguagem C com 4 funções (somar, subatrair, multiplicar e dividir) 
 recebendo os parametros do tipo float A e float B o retorno de cada função deve ser o valor da operação matemática
 2-crie um program em linguagem C que crie uma matriz de 10,3 armazene a nota P1, P2 e MEDIA nas respectivas colunas, 
 crie uma função passando a matriz como parametro para identificar a maiorNotaP1, a maiorNotaP2 e a maiorMedia e mediaSala
-
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 float somar(float a, float b)
 {
@@ -33,33 +34,23 @@ float multiplicar(float a, float b)
     return mult;
 }
 
-    float dividir(float a, float b)
+float dividir(float a, float b)
 {
     float divi;
     divi = a/b;
     return divi;
 }
 
-
 int main()
 {
-    float a,b, rsoma, rsub, rmulti, rdivi;
+    float a = 0, b = 0;
     printf("Digite a\n");
     scanf("%f", &a);
     printf("Digite b\n");
     scanf("%f", &b);
-    
-            rsoma = somar(a,b);
-            printf("Resultado da soma: %.2f", rsoma);
-            
-            rsub = subtrair(a,b);
-            printf("\nResultado da subtracao: %.2f", rsub);
-            
-            rmulti = multiplicar(a,b);
-            printf("\nResultado da multiplicacao: %.2f", rmulti);
-            
-            rdivi = dividir(a,b);
-            printf("\nResultado da divisao: %.2f", rdivi);
-            
+    printf("Resultado da soma: %.2f", somar(a,b));
+    printf("\nResultado da subtracao: %.2f", subtrair(a,b));
+    printf("\nResultado da multiplicacao: %.2f", multiplicar(a,b));
+    printf("\nResultado da divisao: %.2f", dividir(a,b));          
     return 0;
 }
