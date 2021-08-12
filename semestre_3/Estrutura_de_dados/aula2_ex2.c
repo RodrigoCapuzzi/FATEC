18a97b444bf8e57a140201d81d944dec68359853
Rodrigo Sanches Dias Capuzzi <70242653+RodrigoCapuzzi@users.noreply.github.com>
2021-08-11 21:35:59 -0300

    Update aula2_ex2.c

--- semestre_3/Estrutura_de_dados/aula2_ex2.c
+++ semestre_3/Estrutura_de_dados/aula2_ex2.c
@@ -1,42 +1,21 @@
-/*2. Escrever uma função recursiva que retorna o tamanho de um string, tamstring(char s[])*/
+/*
+2- Escrever uma função recursiva que retorna o tamanho de um string, tamstring(char s[])
+*/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int tamstring(char s[])
-{
-    int i=0, soma = 0;
-    if (s[i] == '\n')
-    {
-        //printf("%i", soma);
-        return 1;
-    }
+{   
+    if (s[0] == '\0')
+        return 0;
     else
-    {
-        soma += 1;
-        return tamstring(s+1);
-    }
+        return 1 + tamstring(&s[1]);
 }
 
-
-int main()
-{
-    //int i = 7;
-    char palavra[8];
-    //printf("Digite uma palavra: ");
-    //scanf("%c", &palavra);
-    palavra[0] = 'a';
-    palavra[1] = 'b';
-    palavra[2] = 'a';
-    palavra[3] = 'c';
-    palavra[4] = 'a';
-    palavra[5] = 'x';
-    palavra[6] = 'i';
-    palavra[7] = '\n';
-    for (int i = 0; i < 8; i++)
-    {
-        printf("%c", palavra[i]);
-    }
-    printf("Tamanho da palavra: %i", tamstring(palavra));
-    //printf("Oi");
-    return 0;
+void main()
+{    
+    char s[30];
+    printf("\nDigite a palavra que deseja saber o tamanho: ");
+    scanf("%s", s);
+    printf("\nA palavra '%s' possui %i letras", s, tamstring(s));
 }
